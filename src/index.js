7e8181dbaa2b236168a7941756343bfc938cbf03
Telford Ho <telfordho@gmail.com>
2019-02-08 18:15:10 +0800

    fix mui errors

--- src/index.js
+++ src/index.js
@@ -3,24 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import configureStore from './store';
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import * as serviceWorker from './serviceWorker';
 
-const typographyV1Theme = (theme) => {
-  return createMuiTheme({
-    ...theme,
-    typography: {
-      useNextVariants: false,
-    },
-  });
-}
-
 ReactDOM.render(
-    <MuiThemeProvider theme={typographyV1Theme}>
       <Provider store={configureStore()}>
         <App />
-      </Provider>
-    </MuiThemeProvider>,
+      </Provider>,
     document.getElementById('root'),
   );
 
